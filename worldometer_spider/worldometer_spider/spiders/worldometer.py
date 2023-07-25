598f30159d9fa1990190fd7c83dc6b0a6205d8f2
Jeet <38141300+j-33-t@users.noreply.github.com>
2023-07-25 22:38:33 +0200

    pip freeze

--- worldometer_spider/worldometer_spider/spiders/worldometer.py
+++ worldometer_spider/worldometer_spider/spiders/worldometer.py
@@ -7,4 +7,27 @@ class WorldometerSpider(scrapy.Spider):
     start_urls = ["https://www.worldometers.info/world-population/population-by-country"]
 
     def parse(self, response):
-        pass
+        countries = response.xpath('//td/a')
+
+        for country in countries:
+            country_name = country.xpath(".//text()").get()
+
+            # relative url
+            link = country.xpath(".//@href").get()
+            
+            # extract link with response and parse multiple links
+            yield response.follow(url=link, callback = self.parse_country, meta = {'country': country_name})
+    
+    def parse_country(self,response):
+        country = response.request.meta['country']
+        rows = response.xpath("(//table[contains(@class,'table')])[1]/tbody/tr")
+
+        for row in rows:
+            year = row.xpath(".//td[1]/text()").get()
+            population = row.xpath(".//td[2]/strong/text()").get()
+
+            yield {
+                'country': country,
+                'year':year,
+                'population':population
+            }
